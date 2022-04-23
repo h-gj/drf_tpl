5f1ef42dc9e5862ca5fbf34797156f96a6d8d6c4
h-gj <690631890@qq.com>
2022-04-23 13:19:57 +0800

    feat: handle validation err msg format

--- drf_tpl/exceptions.py
+++ drf_tpl/exceptions.py
@@ -1,11 +1,27 @@
 from django.http import JsonResponse
-from rest_framework.exceptions import APIException
+from rest_framework.exceptions import APIException, ValidationError
 from rest_framework.views import exception_handler
 
 
 def custom_exception_handler(exc, context):
     exception_handler(exc, context)
-    if isinstance(exc, APIException):
+
+    # Order matters, ValidationError subclass APIException, so it should be placed above APIException handling.
+    if isinstance(exc, ValidationError):
+        friendly_msg = ''
+        for f_name, f_err_msg in exc.detail.items():
+            msg = ', '.join(f_err_msg)
+            friendly_msg += (
+                msg
+                .replace('This field', f_name)
+                .replace('This value', 'The value of ' + f_name)
+                .replace('this field', f_name)
+                .replace('this value', 'value of' + f_name)
+            )
+        return JsonResponse({'code': 40000, 'message': friendly_msg, 'data': None})
+
+    elif isinstance(exc, APIException):
         return JsonResponse({'code': exc.get_codes(), 'message': exc.detail, 'data': None})
+
     else:
         return JsonResponse({'code': 50000, 'message': exc.__str__(), 'data': None})
